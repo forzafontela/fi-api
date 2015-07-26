1bbd1fc4af674b15ed036db6d930656a7b34776c
forzafontela <forzafontela@gmail.com>
2015-07-26 01:58:33 -0500

    Fix views (this time it's for real)

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.set('views', './views/');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     res.render('index', { message: '/forzafontela' });
